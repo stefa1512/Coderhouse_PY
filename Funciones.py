c82df53ce0a74815f557a1037dd53ef1b7d4e02e
Stefano DAlfonso <stefanoluigi15@gmail.com>
2023-11-08 17:06:17 -0500

    Guardar el archivo de Funciones.py

---
+++ Funciones.py
@@ -0,0 +1,14 @@
+#def saludar_a_los_estudiantes():
+#    print("hola como estan")
+
+#saludar_a_los_estudiantes()
+
+def saludar_estudiantes(nombre):
+    print(f"Hola como estas {nombre}")
+
+saludar_estudiantes(input("Ingresa un nombre: "))
+
+
+def retornar_saludo_a_estudiante(nombre):
+    mensaje = f"Hola como estar {nombre}"
+    return mensaje
