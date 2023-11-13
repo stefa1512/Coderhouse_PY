9ccbe6aaa9c886eaec899d5b43ea053c68f45e39
Stefano DAlfonso <stefanoluigi15@gmail.com>
2023-11-13 17:41:19 -0500

    agregando ejercicio precio

---
+++ ejercicio_precio.py
@@ -0,0 +1,7 @@
+#Una tienda ofrece descuento del 15% sobre el total de la compra u un cliente desea saber cuanto deberá pagar finalmente por su compra
+
+precio = float(input("Ingrese el precio del producto: "))
+
+precio_nuevo = precio*(1-0.15)
+
+print(f"El nuevo precio con descuento es de: {precio_nuevo} soles")
